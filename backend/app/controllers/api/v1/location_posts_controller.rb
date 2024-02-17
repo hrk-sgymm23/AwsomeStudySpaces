cc09b7aaa95a13a3a5c3c8cc5dc5909a9ca86566
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-02-17 18:40:53 +0900

    feat: add crud

--- backend/app/controllers/api/v1/location_posts_controller.rb
+++ backend/app/controllers/api/v1/location_posts_controller.rb
@@ -1,27 +1,46 @@
 class Api::V1::LocationPostsController < ApplicationController
+    before_action :set_location_post, only: [:show, :update, :destroy]
+
     def index
-        location_posts = LocationPost.all
-        render json: location_posts, status: :200
+        @location_posts = LocationPost.all
+        render json: @location_posts, status: :ok
     end
 
     def show
-        location_post = LocationPost.find(params[:id])
-        render json: location_post, status: :200
+        render json: @location_post, status: :ok
     end
 
     def create
-        location_post = LocationPost.new(post_params)
-        if location_post.save
-            render json: location_post, status: :200
+        @location_post = LocationPost.new(location_post_params)
+        if @location_post.save
+            render json: @location_post, status: :ok
         else
-            render json: location_post.errors, status: :400
+            render json: @location_post.errors, status: :bad_request
+        end
+    end
+
+    def update
+        if @location_post.update(location_post_params)
+            render json: @location_post, status: :ok
+        else
+            render json: @location_post.errors, status: :bad_request
+        end
+    end
+
+    def destroy
+        if @location_post.destroy
+            render json: @location_post, status: :ok
+        else
+            render json: { error: 'Failed to delete resource' }, status: :unprocessable_entity
         end
     end
 
     private
-        def post_params
-            params.require(:location_post).permit(:title, :description, :address)
+        def set_location_post
+            @location_post = LocationPost.find(params[:id])
         end
 
-
+        def location_post_params
+            params.require(:location_post).permit(:title, :description, :address)
+        end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :location_posts
+      resources :location_posts, only: [:index, :show, :create, :update, :destroy]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
