404af60901ea397f948573d38ba7ed5919b9d87f
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-02-23 23:20:57 +0900

    chore: fix boot problem

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,37 +1,19 @@
 import './App.css';
+import React from 'react';
 import { useEffect, useState } from "react";
 import client from './lib/api/client';
 import Header from './components/Header';
-import Body from './components/body';
+// import Body from './components/body';
 import Footer from './components/Footer';
 import Card from './components/Card';
 
-interface LocationPosts {
-  id: number;
-  title: string;
-  description: string;
-}
-
 function App() {
-  const [data, setData] = useState< LocationPosts[] | null >(null);
-  const [error, setError] = useState<string | null>(null);
-  useEffect(() => {
-    client.get<LocationPosts[]>('/location_posts')
-      .then((response) => {
-        const responseData = response.data;
-        setData(responseData);
-      })
-      .catch((error) => {
-        console.error('HTTPリクエストエラー:', error);
-        setError(error.message);
-      });
-  }, []);
 
   return (
     <div className="App">
       <Header />
       <Card />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
