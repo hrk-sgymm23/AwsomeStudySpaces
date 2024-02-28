991cd642369620ba9c78e2a54b4f6167486e666a
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-02-28 23:39:16 +0900

    feat: add devise auth

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  # mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
       resources :location_posts, only: [:index, :show, :create, :update, :destroy]
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
