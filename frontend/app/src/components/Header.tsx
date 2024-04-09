b48bc49e6e8ed09ab6a8b512fd70c0a982ba1eb3
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-04-10 00:20:37 +0900

    chore: fix header style

--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -26,6 +26,39 @@ function Header() {
         }
     }
 
+    // return (
+    //     <header className="body-font text-left flex p-4 m-4">
+    //         <h1 className="text-4xl w-3/4 font-bold">
+    //             <Link to="/LocationPosts" className="hover:text-blue-600 transition-colors duration-300">
+    //                 🧑‍💻Awsome Study Spaces🧑‍💻
+    //             </Link>
+    //         </h1>    
+    //         <div className='flex group body-font text-right text-xl font-bold'>
+    //             <div className='group-hover:visible opacity-100 whitespace-no-wrap'>
+    //                 {isSignedIn && currentUser ? 
+    //                 <>
+    //                     {currentUser && <a>User: {currentUser.name}</a>}
+    //                     <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/UserProfile">UserProfile</a>
+    //                     <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/PostCreate">Post Location</a>
+    //                     {/* <button className="hover:text-blue-600 transition-colors duration-300" type="button" onClick={handleSignOut}>SignOut</button> */}
+    //                     <button
+    //                         className="m-5 font-bold hover:text-blue-800 transition-colors duration-300" // ボタンに適用するクラスを追加
+    //                         type="button"
+    //                         onClick={handleSignOut}
+    //                     >
+    //                         SignOut
+    //                     </button>
+    //                 </>
+    //                 :
+    //                 <>                        
+    //                     <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/SignUp" style={{ margin: '5px' }}>SignUp</a>
+    //                     <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/SignIn" style={{ margin: '5px' }}>SignIn</a>
+    //                 </>
+    //                 }
+    //             </div>
+    //         </div>
+    //     </header>
+    // );
     return (
         <header className="body-font text-left flex p-4 m-4">
             <h1 className="text-4xl w-3/4 font-bold">
@@ -34,15 +67,14 @@ function Header() {
                 </Link>
             </h1>    
             <div className='flex group body-font text-right text-xl font-bold'>
-                <div className='group-hover:visible opacity-100 whitespace-no-wrap'>
+                <div className='group-hover:visible opacity-100 whitespace-no-wrap flex items-center'>
                     {isSignedIn && currentUser ? 
                     <>
-                        {currentUser && <a>User: {currentUser.name}</a>}
-                        <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/UserProfile">UserProfile</a>
-                        <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/PostCreate">Post Location</a>
-                        {/* <button className="hover:text-blue-600 transition-colors duration-300" type="button" onClick={handleSignOut}>SignOut</button> */}
+                        {currentUser && <a className="mr-4">User:{currentUser.name}</a>}
+                        <a className="mr-4 hover:text-blue-600 transition-colors duration-300 whitespace-no-wrap" href="/UserProfile">UserProfile</a>
+                        <a className="mr-4 hover:text-blue-600 transition-colors duration-300 whitespace-no-wrap" href="/PostCreate">PostLocation</a>
                         <button
-                            className="m-5 font-bold hover:text-blue-800 transition-colors duration-300" // ボタンに適用するクラスを追加
+                            className="font-bold hover:text-blue-800 transition-colors duration-300" // ボタンに適用するクラスを追加
                             type="button"
                             onClick={handleSignOut}
                         >
@@ -51,8 +83,8 @@ function Header() {
                     </>
                     :
                     <>                        
-                        <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/SignUp" style={{ margin: '5px' }}>SignUp</a>
-                        <a className="m-5 hover:text-blue-600 transition-colors duration-300" href="/SignIn" style={{ margin: '5px' }}>SignIn</a>
+                        <a className="mr-4 hover:text-blue-600 transition-colors duration-300 whitespace-no-wrap" href="/SignUp" style={{ margin: '5px' }}>SignUp</a>
+                        <a className="mr-4 hover:text-blue-600 transition-colors duration-300 whitespace-no-wrap" href="/SignIn" style={{ margin: '5px' }}>SignIn</a>
                     </>
                     }
                 </div>
