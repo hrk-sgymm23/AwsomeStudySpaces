7ed4b44d9b8d199fbe9e779da187f31153fa892c
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-05-27 21:30:07 +0900

    chore: gitignore

--- frontend/app/src/lib/api/client.ts
+++ frontend/app/src/lib/api/client.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const baseEndpoint = process.env.REACT_APP_BASE_ENDPOINT_URL
 
 const client = axios.create({
-    // baseURL: "http://localhost:3001/api/v1",
     baseURL: baseEndpoint,
 });
 
