a798f927ef801d15d937f8cf1e1a03ec21ac2824
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-07-28 02:16:51 +0900

    feat: add files for stg deploy

--- backend/docker/staging/entorypoint.sh
+++ backend/docker/staging/entorypoint.sh
@@ -2,12 +2,15 @@
 set -e
 
 # sudo service nginx start
-rm -f /api/tmp/pids/server.pid
 
 # bundle exec rails db:create
 # bundle exec rails db:migrate RAILS_ENV=staging
 # bundle exec rails db:seed RAILS_ENV=staging
 # bundle exec rake db:reset
 
+echo "puma start"
+
+rm -f app/tmp/pids/server.pid
+# bundle exec puma -C config/puma.rb
 
 exec "$@"
