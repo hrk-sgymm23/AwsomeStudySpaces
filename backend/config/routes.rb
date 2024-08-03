cb1d3d26bb48033a9a2ad0d213aa07b390b8080e
hrk-sgymm23 <78539910+hrk-sgymm23@users.noreply.github.com>
2024-08-03 16:38:50 +0900

    ECSへのデプロイを設定(Nginx含む) (#32)
    * feat: add dockerfile & conffile
    
    * chore: fix entrypoint.sh & compose
    
    * feat: add files for stg deploy
    
    * feat: add files for stg deploy
    
    * chore: fix dockerfile for stg

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
+      resources :health_check, only: :index
       resources :location_posts, only: [:index, :show, :create, :update, :destroy] do
         member do
           get 'get_users_posts'
--- backend/db/migrate/20240313132311_remove_user_id_id_id_to_location_post.rb
+++ backend/db/migrate/20240313132311_remove_user_id_id_id_to_location_post.rb
@@ -1,6 +1,6 @@
 class RemoveUserIdIdIdToLocationPost < ActiveRecord::Migration[7.0]
   def up
-    remove_column :location_posts, :user_id
-    remove_column :location_posts, :user_id_id
+    # remove_column :location_posts, :user_id
+    # remove_column :location_posts, :user_id_id
   end
 end
