fb49da4836564063a8e5cb237cf6e1f4ecff7a59
hrk-sgymm23 <78539910+hrk-sgymm23@users.noreply.github.com>
2024-08-04 10:27:01 +0900

    chore: fix entrypoint cors (#33)

--- backend/docker/staging/entrypoint.sh
+++ backend/docker/staging/entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 # sudo service nginx start
 
 # bundle exec rails db:create
-bundle exec rails db:migrate RAILS_ENV=staging
-# bundle exec rails db:seed RAILS_ENV=staging
-# bundle exec rake db:reset
+bundle exec rake db:reset
+bundle exec rails db:migrate
 
 echo "puma start"
 
